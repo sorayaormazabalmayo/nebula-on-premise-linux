8733413fd31b526fbb2eb43fd3ab8e3df191f782
s.ormazabal <s.ormazabal@saltosystems.com>
2025-04-15 13:12:49 +0200

    Nebula-on-premise Linux Version 3

--- .scripts/make-release.sh
+++ .scripts/make-release.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Building the binary that is going to be released 
+GOOS=linux GOARCH=amd64 go build -o bin/nebula-on-premise-linux cmd/general-service/main.go  
    
 # Exit script on any error
 set -e
